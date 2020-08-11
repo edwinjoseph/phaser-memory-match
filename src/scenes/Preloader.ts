cd5d28516efeec2d20c19ff933a8701ba87f4c01
Edwin Joseph <edwinj3829@googlemail.com>
2020-08-11 23:11:14 +0100

    feat: preload player animations

--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -14,6 +14,60 @@ export default class Preloader extends Phaser.Scene
 
   create()
   {
+    this.anims.create({
+      key: 'up-idle',
+      frames: [{ key: 'base', frame: 55 }],
+    })
+    this.anims.create({
+      key: 'right-idle',
+      frames: [{ key: 'base', frame: 78 }],
+    })
+    this.anims.create({
+      key: 'down-idle',
+      frames: [{ key: 'base', frame: 52 }],
+    })
+    this.anims.create({
+      key: 'left-idle',
+      frames: [{ key: 'base', frame: 81 }],
+    })
+
+    this.anims.create({
+      key: 'up-walk',
+      frames: this.anims.generateFrameNumbers('base', {
+        start: 55,
+        end: 57,
+      }),
+      frameRate: 8,
+      repeat: -1
+    })
+    this.anims.create({
+      key: 'right-walk',
+      frames: this.anims.generateFrameNumbers('base', {
+        start: 78,
+        end: 80,
+      }),
+      frameRate: 8,
+      repeat: -1
+    })
+    this.anims.create({
+      key: 'down-walk',
+      frames: this.anims.generateFrameNumbers('base', {
+        start: 52,
+        end: 54,
+      }),
+      frameRate: 8,
+      repeat: -1
+    })
+    this.anims.create({
+      key: 'left-walk',
+      frames: this.anims.generateFrameNumbers('base', {
+        start: 81,
+        end: 83,
+      }),
+      frameRate: 8,
+      repeat: -1
+    })
+
     this.scene.start('game')
   }
 }
